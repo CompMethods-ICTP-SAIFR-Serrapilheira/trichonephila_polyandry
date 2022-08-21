3aca76a50fd69400eee560a5b6f8e508fabd9590
izabelsalvi <izabel211@gmail.com>
2022-08-20 23:05:28 -0300

    Adding the correlation chart, and exporting the new tricho_female table

--- R/02_exploratory_analysis.R
+++ R/02_exploratory_analysis.R
@@ -3,17 +3,21 @@
 # Script 2: Exploratory data analysis.
 
 ## Loading packages
+#install.packages("PerformanceAnalytics")
+#install.packages("dplyr")
+#install.packages("tidyr")
 library(dplyr)
 library(tidyr)
+library(PerformanceAnalytics)
 
 ## Loading the processed data
 tricho_female <- read.csv("data/processed/tricho_female.csv")
 tricho_processed <- read.csv("data/processed/tricho_processed.csv")
 
-View(tricho_processed)
-table(tricho_processed$spp) #abundance of each observed species
 
+table(tricho_processed$spp) #abundance of each observed species
 summary(tricho_processed)
+head(tricho_female)
 
 ## Visualizing the frequency of our numerical parameters
 par(mfrow = c(4, 2))
@@ -28,76 +32,76 @@ hist(tricho_female$n_fem_aggregate)
 
 names(tricho_female)
 
-## Calculating the corporeal condition (CC) of the female T. clavipes
-# as the residuals from the regression between the log of their mass
-# and the total size, and treating the CC as a representation of the females fitness.
+## Checking to see if there are any possible correlations between numerical variables
 
-# adding the log mass colum to thetricho female and tricho processed
-tricho_female$log_mass <- log (tricho_female$mass_g, base = 10)
-tricho_processed$log_mass <- log (tricho_processed$mass_g, base = 10)
+# Creating a subset of tricho female with only the numerical data needed
+tricho_female_num <- data.frame(tricho_female$ID_individual, tricho_female$sex, tricho_female$age, tricho_female$web_heigth, tricho_female$n_argy,
+                                tricho_female$n_males, tricho_female$total_length_mm,
+                                tricho_female$cephalot_width_mm, tricho_female$fs_mass, tricho_female$n_fem_aggregate,
+                                tricho_female$log_mass, tricho_female$BC)
 
-# Subset - filtering that the log mass and the comprimento that doesnt have nas
-tricho_female_cc <- tricho_female %>% filter(log_mass != "NA", total_length_mm != "NA")
-nrow(tricho_female_cc)
+head(tricho_female_num)
+quantit_corr <- chart.Correlation(tricho_female_num[,c(4,5,6,7,8,9,10,11,12)], histogram=TRUE, pch=19)
+png(file="figs/quantit_corr.png", width = 2000, height = 2000)
+print(chart.Correlation(tricho_female_num[,c(4,5,6,7,8,9,10,11,12)], histogram=TRUE, pch=19))
+dev.off() # exporting the figure
 
-CC_model  <- lm(log_mass ~ total_length_mm, tricho_female)
-CC <-  as.vector(resid(CC_model))
-
-tricho_female_cc$CC <- CC
-class(tricho_female_cc$CC)
-View(tricho_female_cc)
+## We can see that it doesn't seen to have any clear meaningful correlations. Therefore, we will
+# check some qualitative variables pairwise, utilizing
 
 
-## Remembering our questions and plotting variables to observe their patterns
+## Remembering our questions and plotting variables to check for their relations
 
 ## 1. Interations between T. clavipes and the amount of argyrodes
 
 # 1.1 Presence and absence of Argyrodes and the female
-tricho_female_cc$pres_argy <- tricho_female_cc$n_argy
-tricho_female_cc$pres_argy[tricho_female_cc$pres_argy==2]<-1
-tricho_female_cc$pres_argy[tricho_female_cc$pres_argy==3]<-1
-tricho_female_cc$pres_argy[tricho_female_cc$pres_argy==4]<-1
-unique(tricho_female_cc$pres_argy)
+par(mfrow = c(1,1))
+unique(tricho_female$pres_argy)
+boxplot(tricho_female$BC ~ tricho_female$pres_argy)
 
-tricho_female_cc$pres_argy <- factor(tricho_female_cc$pres_argy)
-
-boxplot(tricho_female_cc$CC ~ tricho_female_cc$pres_argy)
-
-# 1.2 CC Females and the number of Argyrodes
-tricho_female_cc$n_argy <- as.numeric(tricho_female_cc$n_argy)
-boxplot(tricho_female_cc$CC ~ tricho_female_cc$n_argy, ylab = "Female CC", xlab = "N Argyrodes")
+# 1.2 BC Females and the number of Argyrodes
+tricho_female$n_argy <- as.numeric(tricho_female$n_argy)
+boxplot(tricho_female$BC ~ tricho_female$n_argy, ylab = "Female BC", xlab = "N Argyrodes")
 
 # 1.3 Number Argyrodes and the presence of the "Food string"
-class(tricho_female_cc$food_string)
-tricho_female_cc$food_string <- factor(tricho_female_cc$food_string)
-boxplot(tricho_female_cc$n_argy ~ tricho_female_cc$food_string, ylab = "N argyrodes", xlab = "food_string")
+class(tricho_female$food_string)
+tricho_female$food_string <- factor(tricho_female$food_string)
+boxplot(tricho_female$n_argy ~ tricho_female$food_string, ylab = "N argyrodes", xlab = "food_string")
 
 ## 2. Males and females
-# 2.1 CC female and male number
-class(tricho_female_cc$n_males)
-tricho_female_cc$n_males <- as.numeric(tricho_female_cc$n_males)
-boxplot(CC ~ n_males, data = tricho_female_cc, ylab = "N argyrodes", xlab = "food_string")
+# 2.1 BC female and male number
+class(tricho_female$n_males)
+tricho_female$n_males <- as.numeric(tricho_female$n_males)
+boxplot(BC ~ n_males, data = tricho_female, ylab = "BC Female", xlab = "N males")
 
-# 2.2 CC females and presence of males
-tricho_female_cc$males_presence <- tricho_female_cc$n_males
-tricho_female_cc$males_presence[tricho_female_cc$males_presence==2]<-1
-tricho_female_cc$males_presence[tricho_female_cc$males_presence==3]<-1
-unique(tricho_female_cc$males_presence)
-boxplot(CC ~ males_presence, data = tricho_female_cc, ylab = "CC Females", xlab = "Presence of males")
+# 2.2 BC females and presence of males
+#tricho_female$males_presence <- tricho_female$n_males
+#tricho_female$males_presence[tricho_female$males_presence==2]<-1
+#tricho_female$males_presence[tricho_female$males_presence==3]<-1
+unique(tricho_female$males_presence)
+boxplot(BC ~ males_presence, data = tricho_female, ylab = "BC Females", xlab = "Presence of males")
 
 # 2.3 Food string mass and presence of males
-boxplot(fs_mass ~ males_presence, data = tricho_female_cc)
+boxplot(fs_mass ~ males_presence, data = tricho_female)
 
 # 2.4 Number of males and presence or absence of a string
-boxplot(n_males ~ food_string, data = tricho_female_cc)
+boxplot(n_males ~ food_string, data = tricho_female)
 
 ## 3. Number of Argyrodes and males
-boxplot(n_argy ~ n_males, data = tricho_female_cc)
+boxplot(n_argy ~ n_males, data = tricho_female)
 
 ## 4. Agraggates
 
 # 4.1 N Argyrodes agreggate
-boxplot(n_argy ~ aggregate, data = tricho_female_cc)
+boxplot(n_argy ~ aggregate, data = tricho_female)
 
 # 4.2 N males agregate
-boxplot(n_males ~ aggregate, data = tricho_female_cc)
+boxplot(n_males ~ aggregate, data = tricho_female)
+
+
+## Exporting the new table
+write.csv(x = tricho_female,
+          file = "data/processed/tricho_female.csv",
+          row.names = TRUE)
+head(tricho_female)
+
